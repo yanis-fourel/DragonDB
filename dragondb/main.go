0fd1c573123815b76320287651c45cbfe9b00c6b
Yanis Fourel <yanis.fourel@ledr.io>
2023-12-02 15:32:25 +0800

    Listen for commands through TCP

--- dragondb/main.go
+++ dragondb/main.go
@@ -3,17 +3,52 @@ package main
 import (
 	"dragon/store"
 	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
-func main() {
-	s, err := store.New()
-	if err != nil {
-		panic(err)
-	}
+const port = 6969
 
-	s.Set("foo", "bar")
-	s.Set("baz", "qux")
-	fmt.Println(s.Get("foo"))
-	fmt.Println(s.Get("baz"))
-	fmt.Println(s.Get("qux"))
+func main() {
+	store, err := store.New()
+	if err != nil {
+		log.Fatalln("Error creating store: ", err)
+	}
+	defer store.Close()
+
+	l, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatalln("Error listening to TCP port: ", err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatalln("Error accepting connection: ", err)
+		}
+
+		go handleClient(conn, store)
+	}
+}
+
+func handleClient(conn net.Conn, store *store.Store) {
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 129)
+
+		_, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			log.Println("Error reading from TCP channel: ", err)
+			return
+		}
+
+		cmd := string(buf)
+		log.Println("Received command: ", cmd)
+	}
 }
